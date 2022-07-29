e14f4fd8b4cb8f25570e523a918b33fbb1b1cea3
Makale Lord <110202582+MakaleLord@users.noreply.github.com>
2022-07-28 19:52:44 -0500

    Add files via upload

---
+++ game.py
@@ -0,0 +1,56 @@
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Roboto+Mono');
+html{
+   display:flex;
+   justify-content:center;
+   align-items: center;
+   width: 100%;
+   height: 100%;
+   background-image: url('https://source.unsplash.com/collection/327760/1600x900');
+   background-size:cover;
+}
+
+body{
+   width:55%;
+   height:60vh;
+   border:1px solid lightgray;
+   background-color:white;
+   border-radius:8px;
+   font-family: 'Roboto Mono', monospace;
+   box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+   border-top:32px solid #eee;
+   overflow-y:scroll;
+}
+
+body > div{
+  height: calc(100% - 40px);
+  overflow-y:auto;
+  padding: 20px;
+  font-size: 24px;
+  white-space: pre;
+}
+
+pre{
+  margin:inherit;
+  font-family: inherit;
+}
+</style>
+
+<div>
+You choose Rock &#128142;
+Computer choose Rock &#128142;
+It's a draw
+You choose Paper &#128220;
+Computer choose Rock &#128142;
+You won
+You choose Scissors &#9986;
+Computer choose Scissors &#9986;
+It's a draw
+
+
+Match Summary
+Total Matches:  3
+You won:  1
+You lost:  0
+You won the whole thing
---
+++ phonebook.py
@@ -0,0 +1,121 @@
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Roboto+Mono');
+html{
+   display:flex;
+   justify-content:center;
+   align-items: center;
+   width: 100%;
+   height: 100%;
+   background-image: url('https://source.unsplash.com/collection/327760/1600x900');
+   background-size:cover;
+}
+
+body{
+   width:55%;
+   height:60vh;
+   border:1px solid lightgray;
+   background-color:white;
+   border-radius:8px;
+   font-family: 'Roboto Mono', monospace;
+   box-shadow: 0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22);
+   border-top:32px solid #eee;
+   overflow-y:scroll;
+}
+
+body > div{
+  height: calc(100% - 40px);
+  overflow-y:auto;
+  padding: 20px;
+  font-size: 24px;
+  white-space: pre;
+}
+
+pre{
+  margin:inherit;
+  font-family: inherit;
+}
+</style>
+
+<div>
+There are a total of 1000 contacts in file
+
+1 Adena Helble : 02933816104
+2 Adolph Chesser : 09745-819-3 9
+3 Adolph Dinwoodie : 04519361 9359
+4 Agnes Cunning : 042 940 73-28
+5 Ahmad Pioli : 08401436-1
+6 Akiko Iliff : 09284-178 79-72
+7 Al Mestad : 046-10 8587
+8 Alan Gerry : 06354-53491-1
+9 Alan Lotta : 034129552 56
+10 Alana Wehmann : 0847 74618 63
+11 Alanna Nowicki : 041-4122-48
+12 Albertina Vernola : 075-3962-1
+13 Alberto Kolek : 04368248 18
+14 Alec Cornman : 04963-297 2 3
+15 Alejandro Forquer : 04 079-50 17
+16 Alexander Wand : 05893 256-57 58
+17 Alfredia Bridger : 0836-954-65 64
+18 Alina Higgin : 03-685 76-1
+19 Allan Lima : 0132-086-1-62
+20 Allen Bobseine : 0923714-08-01
+21 Alma Guyton : 05739-862-26 61
+22 Alma Hilgefort : 080246 506-81-12
+23 Alonso Magelssen : 073503 198
+24 Alphonso Covey : 07235422-1
+25 Alva Reigner : 021-245 052
+26 Alva Sauvageau : 045-5177 0
+27 Alvaro Colfer : 047535660 48
+28 Alvin Durrani : 0638-263-62-3
+29 Alycia Coultrap : 0208 1048
+30 Amalia Velie : 0233050 4
+31 Amber Anselmi : 001323086-87
+32 America Mielsch : 01545733898
+33 Amie Deslandes : 0572651-60 21
+34 Ana Seltzer : 097802186-239
+35 Anastacia Rackett : 069 98520-21
+36 Anderson Voller : 040507 6432
+37 Andrew Bentley : 078 410 75 63
+38 Andy Mocco : 0890727821457
+39 Andy Seawell : 048226859 04
+40 Angle Speckman : 08467254 63-94
+41 Anjanette Ehmann : 07623 37-5 6
+42 Anjanette Millhouse : 04775627-03
+43 Annamarie Devendorf : 068-631-47
+44 Anne Coad : 01405 907-89-96
+45 Anneliese Blagman : 0859-53 2-59
+46 Anneliese Booker : 017-802-62 56
+47 Annmarie Babula : 0245268-38 02
+48 Antione Cerutti : 08749-47 579
+49 Antione Lastiri : 0037 9804 96
+50 Antionette Bogue : 0163-452 69 6
+51 Antoine Search : 008482665 2
+52 Antone Yeldell : 09684735-35-65
+53 Antonia Maslanka : 00594-83-34 93
+54 Antonio Acothley : 036 359 73-49
+55 Ariana Labor : 091704-31-9832
+56 Ariana Rager : 06-894 382
+57 Arline Burgess : 009-2839169
+58 Armando Nass : 09612 03 2395
+59 Arnold Thronton : 026 693 79-94
+60 Arnoldo Galamay : 0093486 71-1
+61 Arnoldo Seipel : 0542-35209 83
+62 Arron Zari : 031486 70 35
+63 Art Esperanza : 095438918516
+64 Arthur Borchelt : 030568-30-8
+65 Ashanti Hochman : 06139 16 663
+66 Ashley Mckellan : 090 8468-1
+67 Athena Flath : 06293 6035191
+68 Aubrey Creasy : 007562 7915 85
+69 Audrey Dimatteo : 045673 021-13-43
+70 Audrie Smid : 03758295-10 32
+71 Audrie Yiu : 03095-624829
+72 Augustine Crosswell : 0305 20439-38
+73 Augustine Fogelman : 063-73970-62
+74 Augustus Lavallie : 09-715-93-76
+75 Aurelio Steadman : 0701023-8432
+76 Aurore Floris : 014 985428
+77 Ayana Zielinski : 0753-10-85
+
+There are 77 contacts starting with A
